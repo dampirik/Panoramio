a582b80f0cb912c73cb75d2a93f33a3bc8273b4c
Дмитрий Бочкарев < >
2016-07-21 19:38:13 +0300

    ini project

---
+++ Panoramio/Panoramio/Server/ServerFacade.cs
@@ -0,0 +1,10 @@
+﻿namespace Panoramio.Server
+{
+    /// <summary>
+    /// http://www.panoramio.com/api/data/api.html
+    /// </summary>
+    public static class ServerFacade
+    {
+        
+    }
+}
